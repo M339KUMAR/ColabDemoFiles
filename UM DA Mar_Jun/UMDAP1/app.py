a130fb4deebf237fe2d19e0af5d071ccd79e04e7
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-04-16 05:19:57 +0530

    Update Excel file path in app.py

--- UM DA Mar_Jun/UMDAP1/app.py
+++ UM DA Mar_Jun/UMDAP1/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # df = pd.read_excel('/content/sample_data/HHS_Unaccompanied_Alien_Children_Program.xlsx')
-df = pd.read_excel('
+df = pd.read_excel('UM DA Mar_Jun/UMDAP1/HHS_Unaccompanied_Alien_Children_Program.xlsx')
 st.title("Hello from Colab via ngrok")
 st.write("This works!")
 st.dataframe(df)
