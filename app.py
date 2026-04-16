caf9ba7303cf026d9f277309b428688798306101
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-04-16 06:29:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
 import streamlit as st
 import pandas as pd
+import openpyxl
 
-df = pd.read_excel('/content/sample_data/HHS_Unaccompanied_Alien_Children_Program.xlsx')
+#df = pd.read_excel('/content/sample_data/HHS_Unaccompanied_Alien_Children_Program.xlsx')
+df = pd.read_excel('HHS_Unaccompanied_Alien_Children_Program.xlsx', engine='openpyxl')
 st.title("Hello from Colab via ngrok")
 st.write("This works!")
 st.dataframe(df)
