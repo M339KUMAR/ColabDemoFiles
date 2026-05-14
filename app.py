7c10d0253fe03919bef1e3e062fbd525d7473725
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-14 18:13:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -297,6 +297,7 @@ elif granularity == "Yearly":
 # Reset index
 grouped_df = grouped_df.reset_index()
 
+st.write(df.columns.tolist())
 st.write(grouped_df.columns.tolist())
 # =====================================
 # KPI CALCULATIONS
